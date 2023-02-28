8020b551c23b73eda020960e6c8f3ff1df174f2c
jinwon93 <kjw8208@nate.com>
2023-02-28 23:09:21 +0900

    20230228

--- elasticsearch/src/main/java/jin/elasticsearch/member/presentation/MemberController.java
+++ elasticsearch/src/main/java/jin/elasticsearch/member/presentation/MemberController.java
@@ -1,10 +1,17 @@
 package jin.elasticsearch.member.presentation;
 
 
+import jin.elasticsearch.member.application.MemberService;
+import jin.elasticsearch.member.presentation.dto.MemberResponse;
+import jin.elasticsearch.member.presentation.dto.MemberSaveAllRequest;
+import jin.elasticsearch.member.presentation.dto.MemberSaveRequest;
+import jin.elasticsearch.member.presentation.dto.SearchCondition;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,9 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
 
-    @GetMapping("/T")
-    public String hello() {
-        return "T";
+
+    private final MemberService memberService;
+
+    public  ResponseEntity<Void> saveAll(@RequestBody MemberSaveAllRequest memberSaveRequest) {
+        memberService.saveAllMember(memberSaveRequest);
+        return  ResponseEntity.ok().build();
+
     }
 
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberResponse>> searchByName(SearchCondition searchCondition , Pageable pageable) {
+        return ResponseEntity.ok(memberService.searchByCondition(searchCondition , pageable));
+    }
+
+    @GetMapping("/members/nickname/startwith")
+    public ResponseEntity<List<MemberResponse>> findByStartWithNickname(@RequestParam String nickname  , Pageable pageable) {
+        return ResponseEntity.ok(memberService.findByStarWithNickname(nickname , pageable));
+    }
 }
